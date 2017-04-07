e2c515d8171c15ee2adf946654a8e66b7bdecc81
shockeyt <shockeyt@gmail.com>
2017-04-07 16:18:00 -0600

    passed 6 of 11 tests

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  // { _id: 1, task: 'Laundry', description: 'Wash clothes' },
-  // { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  // { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 1, task: 'Laundry', description: 'Wash clothes' },
+  { _id: 2, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
+  { _id: 3, task: 'Homework', description: 'Make this app super awesome!' }
 ];
 
 /**********
@@ -53,18 +53,35 @@ app.get('/api/todos/search', function search(req, res) {
 app.get('/api/todos', function index(req, res) {
   /* This endpoint responds with all of the todos
    */
+   // console.log("HEY");
+   res.json({todos: todos});
 });
 
 app.post('/api/todos', function create(req, res) {
   /* This endpoint will add a todo to our "database"
    * and respond with the newly created todo.
    */
+   // todos.push(req.body);
+
 });
 
 app.get('/api/todos/:id', function show(req, res) {
   /* This endpoint will return a single todo with the
    * id specified in the route parameter (:id)
    */
+   var idChange = req.params.id;
+   var newTodo;
+   // var parseTodo = parseInt(idChange);
+   for (var i = 0; i < todos.length; i++) {
+      if (todos[i]._id == idChange) {
+        console.log(todos[i]._id);
+        console.log(idChange);
+        newTodo =todos[i];
+      }
+   }
+   console.log(idChange);
+   console.log(JSON.stringify(newTodo));
+   res.json({newTodo});
 });
 
 app.put('/api/todos/:id', function update(req, res) {
